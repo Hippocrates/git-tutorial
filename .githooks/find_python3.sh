6eabfa23de27998186f5b384238f45759755719b
Stephen <stephen@braceyourselfgames.com>
2025-03-11 14:23:15 -0700

    Add searching for python 3 to the .sh file

--- .githooks/find_python3.sh
+++ .githooks/find_python3.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 if test -f '/c/Program Files/Epic Games/UE_5.4/Engine/Binaries/ThirdParty/Python3/Win64/python.exe'; then
   export PYTHON3="/c/Program Files/Epic Games/UE_5.4/Engine/Binaries/ThirdParty/Python3/Win64/python.exe"
+elif type python &> /dev/null && python -c "import sys;sys.exit(sys.version_info.major!=3)"; then
+  export PYTHON3="python"
+  echo "HERE"
 elif type python3 &> /dev/null; then
   export PYTHON3="python3"
 else
